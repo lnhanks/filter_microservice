251852e7359f3252d70804992e19d9073da728d1
lnhanks <67074258+lnhanks@users.noreply.github.com>
2023-05-05 13:16:23 -0700

    Add files via upload

---
+++ caller.py
@@ -0,0 +1,6 @@
+URL = "http://localhost:8000/filter/"
+PARAMS = {'species':'dog', 'gender': 'male', 'personality': 'lazy'}
+response = requests.get(url = URL, params = PARAMS)
+
+data = response.json()
+print(data)
